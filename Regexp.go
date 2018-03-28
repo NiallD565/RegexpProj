1fbdc0012942328c4dc8fc2c16f6126c02962b1f
NiallD565 <G00338932@gmit.ie>
2018-03-28 10:15:42 +0100

    comments to thompsons conctruction

--- Regexp.go
+++ Regexp.go
@@ -4,29 +4,35 @@ import (
 	"fmt"
 )
 
+// pointers to other states
 type state struct {
-	symbol rune
+	symbol rune // rune gives the integer value of a character
 	edge1  *state
 	edge2  *state
 }
 
+// keeps track of the initial state and the accept state of the fragment
 type nfa struct {
 	initial *state
 	accept  *state
 }
 
+// uses a stack
 func poregtonfa(postfix string) *nfa {
 	nfastack := []*nfa{}
-
+	// loop through the postfix
+	// uses r to represent the given character from the string
 	for _, r := range postfix {
 		switch r {
-		case '.':
-			// pointers to nfa fragments
-			frag2 := nfastack[len(nfastack)-1] // index of the last item on the stack
-			nfastack = nfastack[:len(nfastack)-1]
-			frag1 := nfastack[len(nfastack)-1] // index of the last item on the stack
+		case '.': //concatination
+			// Array to pointers to nfa fragments
+			// Pops two items off the nfa stack
+			frag2 := nfastack[len(nfastack)-1]    // index of the last item on the stack
+			nfastack = nfastack[:len(nfastack)-1] // : gives everything up not including the lasr item
+			frag1 := nfastack[len(nfastack)-1]    // index of the last item on the stack
 			nfastack = nfastack[:len(nfastack)-1]
 
+			// edge1 points to the initial state of frag2
 			frag1.accept.edge1 = frag2.initial
 
 			// & gives the address of the instance
@@ -47,27 +53,29 @@ func poregtonfa(postfix string) *nfa {
 			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 
 		case '*':
+			// only need to pop 1 frag off the stack because * only works on 1 fragments
 			frag := nfastack[len(nfastack)-1] // index of the last item on the stack
 			nfastack = nfastack[:len(nfastack)-1]
 
 			accept := state{}
 			initial := state{edge1: frag.initial, edge2: &accept}
-			frag.accept.edge1 = frag.initial
+			frag.accept.edge1 = frag.initial // joins the accept state to the initial state
 			frag.accept.edge2 = &accept
 
 			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 
 		default:
+			// no need to pop anthing from the stack
 			accept := state{}
-			initial := state{symbol: r, edge1: &accept}
+			initial := state{symbol: r, edge1: &accept} // set the symbol to r or else it will have its default value of 0
 
 			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 		}
 	}
-	return nfastack[0]
+	return nfastack[0] // nfa return value
 }
 
 func main() {
-	nfa := poregtonfa("ab.c*|")
-	fmt.Println(nfa)
+	nfa := poregtonfa("ab.c*|") // test case representing a regular expression
+	fmt.Println(nfa)            // print out the return value (memory address)
 }
