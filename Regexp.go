14453c958a87e08ae7200b099aed98be215022b3
NiallD565 <G00338932@gmit.ie>
2018-04-04 13:43:52 +0100

    add comments to the shunting yard

--- Regexp.go
+++ Regexp.go
@@ -98,18 +98,26 @@ func addstate(l []*state, s *state, a *state) []*state {
 /*
 func intopost(infix string) string {
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+	// * 0 or more
+	// + 1 or more
+	// . concatenate
+	// | or
 
 	postfix := []rune{}
 	s := []rune{}
 
-	for _, r := range infix {
+
+	// ( added to stack
+	// ) add each character stack to the postfix while the opening tag not found get rid of the bracket
+	//
+	for _, r := range infix { // convert string array to runes
 		switch {
 		case r == '(':
 			s = append(s, r)
 		case r == ')':
 			for s[len(s)-1] != '(' {
 				postfix = append(postfix, s[len(s)-1])
-				s = s[:len(s)-1]
+				s = s[:len(s)-1] // get everything but the last element in the list
 			}
 			s = s[:len(s)-1]
 		case specials[r] > 0:
@@ -122,8 +130,8 @@ func intopost(infix string) string {
 			postfix = append(postfix, r)
 		}
 	}
-	for len(s) > 0 {
-		postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
+	for len(s) > 0 { // if any characters are left append them to the string and clear the stack
+		postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1] // get everything but the last element in the list
 	}
 
 	return string(postfix)
