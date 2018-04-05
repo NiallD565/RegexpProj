eab151a16e670a5e6734f389fc83240700e4fa2e
NiallD565 <G00338932@gmit.ie>
2018-04-05 09:57:30 +0100

    go isn't OO based

--- Regexp.go
+++ Regexp.go
@@ -17,6 +17,64 @@ type nfa struct {
 	accept  *state
 }
 
+/*
+func PrintS(s string) {
+	fmt.Println(s)
+}
+
+// Input take input from the user and assigns it to s which is needed by the nfa
+func Input(s string) string {
+	var Input string
+	PrintS(s)
+	fmt.Scan(&Input)
+	return Input
+
+}
+*/
+
+//======================================================================================================================
+/*
+func intopost(infix string) string {
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+	// * 0 or more
+	// + 1 or more
+	// . concatenate
+	// | or
+
+	postfix := []rune{}
+	s := []rune{}
+
+	// ( added to stack
+	// ) add each character stack to the postfix while the opening tag not found get rid of the bracket
+	for _, r := range infix { // convert string array to runes
+		switch {
+		case r == '(':
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				postfix = append(postfix, s[len(s)-1])
+				s = s[:len(s)-1] // get everything but the last element in the list
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				postfix = append(postfix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			postfix = append(postfix, r)
+		}
+	}
+	for len(s) > 0 { // if any characters are left append them to the string and clear the stack
+		postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1] // get everything but the last element in the list
+	}
+
+	return string(postfix)
+}*/
+
+//=====================================================================================================================
+
 // uses a stack
 func poregtonfa(postfix string) *nfa {
 	nfastack := []*nfa{}
